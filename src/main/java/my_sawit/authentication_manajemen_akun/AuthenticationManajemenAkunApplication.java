0a3dc6b362b18de8fe1ab2924a5845c83a9c09a7
Evanlikescode <evanharyo258@gmail.com>
2026-02-19 20:35:54 +0700

    setup project

---
+++ src/main/java/my_sawit/authentication_manajemen_akun/AuthenticationManajemenAkunApplication.java
@@ -0,0 +1,13 @@
+package my_sawit.authentication_manajemen_akun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationManajemenAkunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationManajemenAkunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my_sawit/authentication_manajemen_akun/AuthenticationManajemenAkunApplicationTests.java
@@ -0,0 +1,13 @@
+package my_sawit.authentication_manajemen_akun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManajemenAkunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
