1c9e2296546a53fec06636bd1c157e23824e1e62
evanhwz <evan.haryo@ui.ac.id>
2026-02-19 21:11:03 +0700

    feat: add register to auth service

---
+++ src/main/java/my_sawit/authentication_manajemen_akun/service/AuthService.java
@@ -0,0 +1,27 @@
+package my_sawit.authentication_manajemen_akun.service;
+
+import lombok.RequiredArgsConstructor;
+import my_sawit.authentication_manajemen_akun.dto.RegisterRequest;
+import my_sawit.authentication_manajemen_akun.model.User;
+import my_sawit.authentication_manajemen_akun.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepo;
+
+    public User register(RegisterRequest request){
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .role(request.getRole())
+                .build();
+        return userRepo.save(user);
+
+    }
+
+}
