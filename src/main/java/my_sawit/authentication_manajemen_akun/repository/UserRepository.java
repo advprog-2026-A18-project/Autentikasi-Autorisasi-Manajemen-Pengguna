a603dc35a4a7b584ecd0daff23d69f0ca4b247ad
evanhwz <evan.haryo@ui.ac.id>
2026-02-19 21:08:07 +0700

    feat: add UserRepo

---
+++ src/main/java/my_sawit/authentication_manajemen_akun/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package my_sawit.authentication_manajemen_akun.repository;
+
+import my_sawit.authentication_manajemen_akun.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
