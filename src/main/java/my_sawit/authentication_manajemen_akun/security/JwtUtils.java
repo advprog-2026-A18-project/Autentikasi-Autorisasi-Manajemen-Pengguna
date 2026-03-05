1dec8ebe3b246767c2aa7d3c3a1b337fd79de5fb
evanhwz <evan.haryo@ui.ac.id>
2026-03-06 04:26:45 +0700

    fix: change the getter/setter of jwt using the 0.12.6 version

--- src/main/java/my_sawit/authentication_manajemen_akun/security/JwtUtils.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/security/JwtUtils.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
@@ -19,16 +20,16 @@ public class JwtUtils {
     private int jwtExpirationMs;
 
     private Key getSigningKey(){
-        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(String email, String role){
         return Jwts.builder()
-                .setSubject(email)
+                .subject(email)
                 .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs ))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs ))
+                .signWith(getSigningKey())
                 .compact();
     }
 
