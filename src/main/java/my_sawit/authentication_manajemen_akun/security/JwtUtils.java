9ef705031bd27a01457850eb8c3f5daaea4e2cbf
evanhwz <evan.haryo@ui.ac.id>
2026-03-05 19:29:05 +0700

    feat: add securityconfig and jwt

---
+++ src/main/java/my_sawit/authentication_manajemen_akun/security/JwtUtils.java
@@ -0,0 +1,36 @@
+package my_sawit.authentication_manajemen_akun.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationMs}")
+    private int jwtExpirationMs;
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(String email, String role){
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs ))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+}
