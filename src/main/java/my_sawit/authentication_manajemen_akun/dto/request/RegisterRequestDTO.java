180276fde7ea98d8981288734e9300bcd2448b35
evanhwz <evan.haryo@ui.ac.id>
2026-03-05 19:27:32 +0700

    fix: add new message

--- src/main/java/my_sawit/authentication_manajemen_akun/dto/request/RegisterRequestDTO.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/dto/request/RegisterRequestDTO.java
@@ -14,24 +14,24 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegisterRequestDTO {
 
-    @NotBlank(message = "Username should be filled")
+    @NotBlank(message = "Username should be filled!")
     private String username;
 
-    @NotBlank(message = "Fullname should be filled")
+    @NotBlank(message = "Fullname should be filled!")
     private String fullname;
 
-    @NotBlank(message = "Email should be filled")
-    @Email(message = "Email format is invalid")
+    @NotBlank(message = "Email should be filled!")
+    @Email(message = "Email format is invalid!")
     private String email;
 
-    @NotBlank(message = "Password tidak boleh kosong")
+    @NotBlank(message = "Password should be filled!")
     @Pattern(
         regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$",
         message = "Password must have a minimum of 8 characters and include an uppercase letter, a lowercase letter, and a number."
     )
     private String password;
 
-    @NotBlank(message = "Role should be filled")
+    @NotBlank(message = "Role should be filled!")
     private String role;
 
     private String nomorSertifikasi;
