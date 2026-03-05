f35261efb6a4375117432855dd003e823a8d4f4a
evanhwz <evan.haryo@ui.ac.id>
2026-03-05 17:25:52 +0700

    feat: add response dto

---
+++ src/main/java/my_sawit/authentication_manajemen_akun/dto/response/ApiResponse.java
@@ -0,0 +1,16 @@
+package my_sawit.authentication_manajemen_akun.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+    private int statusCode;
+    private String message;
+    private T data; // isinya adalah AuthResponseDTO dan sejenisnya
+}
---
+++ src/main/java/my_sawit/authentication_manajemen_akun/dto/response/UserResponseDTO.java
@@ -0,0 +1,21 @@
+package my_sawit.authentication_manajemen_akun.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+    private UUID id;
+    private String username;
+    private String fullname;
+    private String email;
+    private String role;
+    private String nomorSertifikasi; // null if user not mandor
+}
