4ab74c63b5e76d6d456f7ad1883f8a870beae231
evanhwz <evan.haryo@ui.ac.id>
2026-03-06 08:53:03 +0700

    feat: add google auth controller

--- src/main/java/my_sawit/authentication_manajemen_akun/controller/AuthController.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/controller/AuthController.java
@@ -2,11 +2,13 @@ package my_sawit.authentication_manajemen_akun.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import my_sawit.authentication_manajemen_akun.dto.request.GoogleAuthRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.request.LoginRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.request.RegisterRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.response.ApiResponse;
 import my_sawit.authentication_manajemen_akun.dto.response.AuthResponseDTO;
 import my_sawit.authentication_manajemen_akun.service.AuthStrategy;
+import my_sawit.authentication_manajemen_akun.service.GoogleAuthServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthStrategy authStrategy;
+    private final GoogleAuthServiceImpl googleAuthServiceImpl;
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<AuthResponseDTO>> register(@Valid @RequestBody RegisterRequestDTO request){
@@ -30,6 +33,18 @@ public class AuthController {
 
         ApiResponse<AuthResponseDTO> response = authStrategy.login(request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/google")
+    public ResponseEntity<ApiResponse<AuthResponseDTO>> googleLogin(@Valid @RequestBody GoogleAuthRequestDTO request) {
+
+        ApiResponse<AuthResponseDTO> response = googleAuthServiceImpl.authenticate(request);
+
+        if (response.getStatusCode() == 200) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(response.getStatusCode()).body(response);
+        }
     }
 
 }
