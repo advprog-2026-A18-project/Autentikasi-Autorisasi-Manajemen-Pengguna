7669aa2c929a1ed6f68dabdabf821f238bd635c2
evanhwz <evan.haryo@ui.ac.id>
2026-03-06 19:15:03 +0700

    resolve: remove exception

--- src/main/java/my_sawit/authentication_manajemen_akun/security/SecurityConfig.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable) // NOSONAR: Secured because session using JWT (Stateless)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
