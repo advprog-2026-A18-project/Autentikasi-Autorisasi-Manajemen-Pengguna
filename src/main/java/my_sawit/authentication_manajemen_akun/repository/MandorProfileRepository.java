f8cc3ff64ff90de61b3436bf298f7a16533c7141
evanhwz <evan.haryo@ui.ac.id>
2026-03-06 07:18:18 +0700

    feat: add finding mandor by username

--- src/main/java/my_sawit/authentication_manajemen_akun/repository/MandorProfileRepository.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/repository/MandorProfileRepository.java
@@ -1,14 +1,16 @@
 package my_sawit.authentication_manajemen_akun.repository;
 
 import my_sawit.authentication_manajemen_akun.model.MandorProfile;
+import my_sawit.authentication_manajemen_akun.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MandorProfileRepository extends JpaRepository<MandorProfile, UUID> {
 
     boolean existsByNomorSertifikasi(String nomorSertifikasi);
-
+    Optional<MandorProfile> findByUser(User user);
 }
