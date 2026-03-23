40e62d86095d09339a6806482d4610613381b247
evanhwz <evan.haryo@ui.ac.id>
2026-03-24 01:38:18 +0700

    [REFACTOR] add findByUser

--- src/main/java/my_sawit/authentication_manajemen_akun/repository/RefreshTokenRepository.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/repository/RefreshTokenRepository.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
     Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUser(User user);
 
     @Modifying
     void deleteByUser(User user);
