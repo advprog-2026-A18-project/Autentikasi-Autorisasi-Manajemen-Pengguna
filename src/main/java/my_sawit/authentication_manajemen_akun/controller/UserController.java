faaa7bac89b0b2cb7f7f6823105ab45067784578
evanhwz <evan.haryo@ui.ac.id>
2026-04-17 06:39:45 +0700

    [REFACTOR] change endpoint of users

--- src/main/java/my_sawit/authentication_manajemen_akun/controller/UserController.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
