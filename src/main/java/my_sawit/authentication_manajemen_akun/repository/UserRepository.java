549ca0045ab669f8e60230e3a616c27f2c96b80b
evanhwz <evan.haryo@ui.ac.id>
2026-04-16 17:21:46 +0700

    [GREEN] add find every user by mandor

--- src/main/java/my_sawit/authentication_manajemen_akun/repository/UserRepository.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/repository/UserRepository.java
@@ -4,10 +4,12 @@ import my_sawit.authentication_manajemen_akun.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,4 +32,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
                            @Param("email") String email,
                            @Param("roleName") String roleName,
                            Pageable pageable);
+
+    List<User> findByMandor(User mandor);
+
 }
