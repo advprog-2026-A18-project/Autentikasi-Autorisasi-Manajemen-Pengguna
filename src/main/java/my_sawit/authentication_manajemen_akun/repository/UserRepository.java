cdefb4a4b02ed68b776a05fff11a52e6459daccb
evanhwz <evan.haryo@ui.ac.id>
2026-04-16 17:26:46 +0700

    [REFACTOR] deleting lib modifying coz it is not used

--- src/main/java/my_sawit/authentication_manajemen_akun/repository/UserRepository.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/repository/UserRepository.java
@@ -4,7 +4,6 @@ import my_sawit.authentication_manajemen_akun.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
