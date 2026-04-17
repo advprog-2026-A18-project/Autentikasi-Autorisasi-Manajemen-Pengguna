f03f1607d3862fa0f61377676bce5adad792e503
evanhwz <evan.haryo@ui.ac.id>
2026-04-17 07:07:47 +0700

    [GREEN] add endpoint of update profile

--- src/main/java/my_sawit/authentication_manajemen_akun/controller/UserController.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/controller/UserController.java
@@ -1,13 +1,13 @@
 package my_sawit.authentication_manajemen_akun.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import my_sawit.authentication_manajemen_akun.dto.request.UserUpdateRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.response.ApiResponse;
 import my_sawit.authentication_manajemen_akun.dto.response.UserResponseDTO;
 import my_sawit.authentication_manajemen_akun.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -27,6 +27,18 @@ public class UserController {
                 200,
                 "Berhasil mengambil profil",
                 profile
+        ));
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<ApiResponse<UserResponseDTO>> updateMyProfile(Principal principal, @Valid @RequestBody UserUpdateRequestDTO request) {
+
+        UserResponseDTO updatedProfile = userService.updateMyProfile(principal.getName(), request);
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                200,
+                "Berhasil memperbarui profil",
+                updatedProfile
         ));
     }
 }
