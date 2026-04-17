8813ce2d8dfe9eecd75f05e853e9d624adb9944d
evanhwz <evan.haryo@ui.ac.id>
2026-04-17 07:02:32 +0700

    [GREEN] add contract update profile

--- src/main/java/my_sawit/authentication_manajemen_akun/service/UserService.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/service/UserService.java
@@ -1,7 +1,9 @@
 package my_sawit.authentication_manajemen_akun.service;
 
+import my_sawit.authentication_manajemen_akun.dto.request.UserUpdateRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.response.UserResponseDTO;
 
 public interface UserService {
     UserResponseDTO getMyProfile(String email);
+    UserResponseDTO updateMyProfile(String email, UserUpdateRequestDTO request);
 }
