70c2a842ab4550f3ddcd4722c871b9487001f967
evanhwz <evan.haryo@ui.ac.id>
2026-05-08 10:45:09 +0700

    fix: rename to ConvertResponseHandler and implement overload so it unecessary to always put mandorProfile repo every time using the method

--- src/main/java/my_sawit/authentication_manajemen_akun/helper/ConvertResponseHandler.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/helper/ConvertResponseHandler.java
@@ -1,6 +1,5 @@
 package my_sawit.authentication_manajemen_akun.helper;
 
-import lombok.RequiredArgsConstructor;
 import my_sawit.authentication_manajemen_akun.dto.response.UserResponseDTO;
 import my_sawit.authentication_manajemen_akun.model.MandorProfile;
 import my_sawit.authentication_manajemen_akun.model.User;
@@ -8,10 +7,9 @@ import my_sawit.authentication_manajemen_akun.repository.MandorProfileRepository
 
 import java.util.Optional;
 
-public class ConvertHandler {
+public class ConvertResponseHandler {
 
-
-    public static UserResponseDTO convertToResponseDTO(User user, MandorProfileRepository mandorProfileRepository) {
+    public static UserResponseDTO convertToUserResponseDTO(User user, MandorProfileRepository mandorProfileRepository ) {
         String nomorSertifikasi = null;
 
         if (user.getRole() != null && "MANDOR".equalsIgnoreCase(user.getRole().getName())) {
@@ -30,6 +28,21 @@ public class ConvertHandler {
                 .email(user.getEmail())
                 .role(user.getRole() != null ? user.getRole().getName() : null)
                 .nomorSertifikasi(nomorSertifikasi)
+                .namaMandor(namaMandor)
+                .build();
+    }
+
+    public static UserResponseDTO convertToUserResponseDTO(User user) {
+
+        String namaMandor = (user.getMandor() != null) ? user.getMandor().getFullname() : null;
+
+        return UserResponseDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .fullname(user.getFullname())
+                .email(user.getEmail())
+                .role(user.getRole() != null ? user.getRole().getName() : null)
+                .nomorSertifikasi(null)
                 .namaMandor(namaMandor)
                 .build();
     }
