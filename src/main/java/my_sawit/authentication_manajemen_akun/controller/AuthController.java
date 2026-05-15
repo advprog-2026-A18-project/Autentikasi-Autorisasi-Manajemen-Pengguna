d173f834d9ce09561b1befadf42ce26e2ab860f6
evanhwz <evan.haryo@ui.ac.id>
2026-05-15 13:20:50 +0700

    [REFACTOR] change the way return response data to be handled fully in services for the auth (excluded refresh token - still needs to be fixed)

--- src/main/java/my_sawit/authentication_manajemen_akun/controller/AuthController.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/controller/AuthController.java
@@ -8,9 +8,7 @@ import my_sawit.authentication_manajemen_akun.dto.request.RegisterRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.request.TokenRefreshRequestDTO;
 import my_sawit.authentication_manajemen_akun.dto.response.ApiResponse;
 import my_sawit.authentication_manajemen_akun.dto.response.AuthResponseDTO;
-import my_sawit.authentication_manajemen_akun.service.AuthStrategy;
-import my_sawit.authentication_manajemen_akun.service.GoogleAuthServiceImpl;
-import my_sawit.authentication_manajemen_akun.service.RefreshTokenServiceImpl;
+import my_sawit.authentication_manajemen_akun.service.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,42 +19,39 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthStrategy authStrategy;
-    private final GoogleAuthServiceImpl googleAuthServiceImpl;
-    private final RefreshTokenServiceImpl refreshTokenServiceImpl;
+    private final LocalAuthService localAuthService;
+    private final OAuthService<GoogleAuthRequestDTO> googleAuthService;
+
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<AuthResponseDTO>> register(@Valid @RequestBody RegisterRequestDTO request){
-        ApiResponse<AuthResponseDTO> response = authStrategy.register(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        ApiResponse<AuthResponseDTO> response = localAuthService.register(request);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<AuthResponseDTO>> login(@Valid @RequestBody LoginRequestDTO request) {
-
-        ApiResponse<AuthResponseDTO> response = authStrategy.login(request);
-        return ResponseEntity.ok(response);
+        ApiResponse<AuthResponseDTO> response = localAuthService.login(request);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/google")
     public ResponseEntity<ApiResponse<AuthResponseDTO>> googleLogin(@Valid @RequestBody GoogleAuthRequestDTO request) {
-
-        ApiResponse<AuthResponseDTO> response = googleAuthServiceImpl.authenticate(request);
+        ApiResponse<AuthResponseDTO> response = googleAuthService.authenticate(request);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse<AuthResponseDTO>> refresh(@Valid @RequestBody TokenRefreshRequestDTO request) {
-        AuthResponseDTO authData = refreshTokenServiceImpl.refreshAccessToken(request.getRefreshToken());
-
+        AuthResponseDTO authData = refreshTokenService.refreshAccessToken(request.getRefreshToken());
         return ResponseEntity.ok(new ApiResponse<>(200, "Token refreshed successfully", authData));
     }
 
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<Void>> logout(@Valid @RequestBody TokenRefreshRequestDTO request) {
 
-        refreshTokenServiceImpl.deleteByToken(request.getRefreshToken());
+        refreshTokenService.deleteByToken(request.getRefreshToken());
         return ResponseEntity.ok(new ApiResponse<>(200, "Successfully logout", null));
     }
 
