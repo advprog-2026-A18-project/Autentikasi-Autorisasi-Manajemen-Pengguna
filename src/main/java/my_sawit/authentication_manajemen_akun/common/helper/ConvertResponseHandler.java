a2da447491c01a6803760183f94cf3496948b527
evanhwz <evan.haryo@ui.ac.id>
2026-05-18 20:49:58 +0700

    [REFACTOR] extract userresponsedto converter to mapper / non-static component

--- src/main/java/my_sawit/authentication_manajemen_akun/common/helper/ConvertResponseHandler.java
+++ src/main/java/my_sawit/authentication_manajemen_akun/common/helper/ConvertResponseHandler.java
@@ -2,54 +2,12 @@ package my_sawit.authentication_manajemen_akun.common.helper;
 
 import my_sawit.authentication_manajemen_akun.dto.response.AuthResponseDTO;
 import my_sawit.authentication_manajemen_akun.dto.response.UserResponseDTO;
-import my_sawit.authentication_manajemen_akun.domain.model.MandorProfile;
 import my_sawit.authentication_manajemen_akun.domain.model.RefreshToken;
 import my_sawit.authentication_manajemen_akun.domain.model.User;
-import my_sawit.authentication_manajemen_akun.domain.repository.MandorProfileRepository;
 import my_sawit.authentication_manajemen_akun.security.JwtUtils;
 import my_sawit.authentication_manajemen_akun.auth.service.RefreshTokenService;
 
-import java.util.Optional;
-
 public class ConvertResponseHandler {
-
-    public static UserResponseDTO convertToUserResponseDTO(User user, MandorProfileRepository mandorProfileRepository ) {
-        String nomorSertifikasi = null;
-
-        if (user.getRole() != null && "MANDOR".equalsIgnoreCase(user.getRole().getName())) {
-            Optional<MandorProfile> profile = mandorProfileRepository.findByUser(user);
-            if (profile.isPresent()) {
-                nomorSertifikasi = profile.get().getNomorSertifikasi();
-            }
-        }
-
-        String namaMandor = (user.getMandor() != null) ? user.getMandor().getFullname() : null;
-
-        return UserResponseDTO.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .fullname(user.getFullname())
-                .email(user.getEmail())
-                .role(user.getRole() != null ? user.getRole().getName() : null)
-                .nomorSertifikasi(nomorSertifikasi)
-                .namaMandor(namaMandor)
-                .build();
-    }
-
-    public static UserResponseDTO convertToUserResponseDTO(User user) {
-
-        String namaMandor = (user.getMandor() != null) ? user.getMandor().getFullname() : null;
-
-        return UserResponseDTO.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .fullname(user.getFullname())
-                .email(user.getEmail())
-                .role(user.getRole() != null ? user.getRole().getName() : null)
-                .nomorSertifikasi(null)
-                .namaMandor(namaMandor)
-                .build();
-    }
 
     public static AuthResponseDTO convertToAuthResponseDTO(
             User user,
